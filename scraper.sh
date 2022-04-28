0b361765daf983bbf5870fedb98cc4e836e23a77
Saniya Maheshwari <saniya.mah@gmail.com>
2022-04-28 19:31:26 +0530

    Download function will return exit code 0
    Also improved formatting of a comment.

--- scraper.sh
+++ scraper.sh
@@ -49,13 +49,18 @@ download_wnid() {
 
 	cd "$wnid"
 
-	# retrieve a list of image URLs from image-net.org,
-	# retain only the flickr URLs (as they are the most reliable),
-	# convert the http connections to https (if we use the raw http URLs
-	# a redirect with the same effect almost always takes place),
-	# and follow these URLs to download images.
+	# retrieve a list of image URLs from image-net.org, retain only the flickr URLs (as they are
+	# the most reliable), convert the http connections to https (if we use the raw http URLs
+	# a redirect with the same effect almost always takes place), and follow these URLs to download
+	# images.
 	wget -q -i <(wget "https://www.image-net.org/api/imagenet.synset.geturls?wnid=$wnid" -q -O - | \
 		grep -m "$num" 'flickr' | sed 's/http/https/')
+
+	# we will observe that wget almost always returns a non-zero exit code (a code of 8, in fact) due
+	# to 404 and other errors caused by missing images.
+	# however, we don't want to this to be registered as an error per-se, hence we exit with code 0
+	# to indicate success.
+	return 0
 }
 
 export -f download_wnid
